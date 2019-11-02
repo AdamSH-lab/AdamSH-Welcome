e7a99726b7c46f03ac9262b9628cc5482c91e96a
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-11-02 19:35:04 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@
 AddEventHandler('playerSpawned', function(spawn)
     local adam = GetPlayerName(soruce) --[הפונקציה שלוקחת את השם של השחקן]
     alert('~g~Welcome To AdamSH Server')
-    notify("~b~Welcome ~g~".. GetPlayerName(soruce) .. " ~b~to AdamSH Server")              --[אין לשנות שורה זאת אם אתם לא יודעים מה אתם עושים]
+    notify("~b~Welcome ~g~".. adam .. " ~b~to AdamSH Server")              --[אין לשנות שורה זאת אם אתם לא יודעים מה אתם עושים]
 end)
     
 function notify(string)
