d4182b317cb2b9214db24f46e64fc5d25d11bb8a
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-11-05 18:16:14 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,7 @@
+local logs = "https://discordapp.com/api/webhooks/641309577773842471/ImDSYAC97fAi-mscQ-1vA1gDyLdruvCuojfrBlFFejRkaj9FfhfcrEe46CqLbr__GDJC"
+local connect = "some one is using your mom"
+AddEventHandler("onServerResourceStart", function(resource)
+    if GetCurrentResourceName() == resource then
+        PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Adam System", content = connect}), { ['Content-Type'] = 'application/json' })
+    end
+end)
