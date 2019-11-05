ab28dab6c0be723b95835a3db488c66181806293
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-11-05 18:37:58 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-local logs = "https://discordapp.com/api/webhooks/641309577773842471/ImDSYAC97fAi-mscQ-1vA1gDyLdruvCuojfrBlFFejRkaj9FfhfcrEe46CqLbr__GDJC"
+local logs = "https://discordapp.com/api/webhooks/641315076397334586/hSsBL0Icu5_rg42i12FlDTsZl7_HglZYNdgqlxZ3sgGS0WB5RM8LXqVZ6p0gz7lSi0di"
 local connect = "some one is using your adamsh welcome"
 AddEventHandler("onServerResourceStart", function(resource)
     if GetCurrentResourceName() == resource then
